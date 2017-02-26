dd94b87a6f1fd58137d128f78d4d81742310c3a5
0smium <steve.walsh@transfueler.com>
2017-02-25 16:14:26 -0800

    got three random images generating smoothely, now to work on event listener

--- app.js
+++ app.js
@@ -16,19 +16,30 @@ for (var i = 0; i < listOfProducts.length; i++) {
 }
 
 var populate = function () {
+
+  var rand = [];
+  while (rand.length < 3) {
+    var randNum = (Math.ceil(Math.random() * listOfProducts.length)) - 1;
+    if (rand.indexOf(randNum) === -1) {
+      rand.push(randNum);
+    }
+    continue;
+  }
+  console.log(rand)
+
   var first = document.getElementById('first');
   var img0 = document.createElement('img');
-  img0.src = listOfProductObjects[0].path
+  img0.src = listOfProductObjects[rand[0]].path
   first.appendChild(img0);
 
   var second = document.getElementById('second');
   var img1 = document.createElement('img');
-  img1.src = listOfProductObjects[1].path
+  img1.src = listOfProductObjects[rand[1]].path
   second.appendChild(img1);
 
   var third = document.getElementById('third');
   var img2 = document.createElement('img');
-  img2.src = listOfProductObjects[2].path
+  img2.src = listOfProductObjects[rand[2]].path
   third.appendChild(img2);
 };
 populate()
