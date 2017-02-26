4ad30f8e96c4913890ee1d4f0a801864eb504e97
0smium <steve.walsh@transfueler.com>
2017-02-25 16:42:34 -0800

    created event listener and log results to console

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 var totalVotes = 0;
 var listOfProducts = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 var listOfProductObjects = [];
+var container = document.getElementById('container')
 
-var product = function (name, path) {
+function Product(name, path) {
   this.name = name; //name of product/image
   this.path = path; //relative path to jpg
   this.votes = 0; //clicks for this product
@@ -12,10 +13,10 @@ var product = function (name, path) {
 };
 
 for (var i = 0; i < listOfProducts.length; i++) {
-  new product(listOfProducts[i], './assets/' + listOfProducts[i] + '.jpg');
+  new Product(listOfProducts[i], './assets/' + listOfProducts[i] + '.jpg');
 }
 
-var populate = function () {
+function populate() {
 
   var rand = [];
   while (rand.length < 3) {
@@ -25,21 +26,45 @@ var populate = function () {
     }
     continue;
   }
-  console.log(rand)
+  // console.log(rand)
 
   var first = document.getElementById('first');
   var img0 = document.createElement('img');
-  img0.src = listOfProductObjects[rand[0]].path
+  img0.src = listOfProductObjects[rand[0]].path;
+  img0.id = listOfProductObjects[rand[0]].name;
   first.appendChild(img0);
 
   var second = document.getElementById('second');
   var img1 = document.createElement('img');
   img1.src = listOfProductObjects[rand[1]].path
+  img1.id = listOfProductObjects[rand[1]].name;
   second.appendChild(img1);
 
   var third = document.getElementById('third');
   var img2 = document.createElement('img');
   img2.src = listOfProductObjects[rand[2]].path
+  img2.id = listOfProductObjects[rand[2]].name;
   third.appendChild(img2);
-};
-populate()
+}
+
+function onClick(click) {
+  console.log('onClick', click);
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+container.addEventListener('click', onClick);
+
+populate();
